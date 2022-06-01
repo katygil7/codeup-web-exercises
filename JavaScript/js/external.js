c675cf9fcbb8f85223be918da294545332fc19a1
katygil7 <gil.katherine22@gmail.com>
2022-06-01 17:00:48 -0500

    HTML/JS Lecture

---
+++ JavaScript/js/external.js
@@ -0,0 +1,25 @@
+console.log("Hello from external JavaScript")
+//alert("Welcome to my Website!")
+//prompt ("What is your favorite color?" )
+//alert ("Great, blue is my favorite color too")
+
+var littleM = prompt("how many days do you want to rent littleM", 3);
+var brotherB = prompt ("how many days do you want to rent brotherB  ", 5);
+var herculesM = prompt("how many days do you want to rent herculesM", 1);
+//function totalPrice (){
+    //var movies = (littleM, brotherB, herculesM)
+    //var price = 3
+    //var grantTotal((littleM * price) + (brotherB * price) + (herculesM * price));
+ //totalPrice()
+//}
+
+var googlePay = 400*6
+var facebookPay = 350*10
+var amazonPay = 380*4
+function totalPay = (googlePay + facebookPay + amazonPay)
+
+
+
+
+
+
