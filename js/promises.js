27e368ab47bb4bcd23b6791e421c2f36863e685f
katygil7 <gil.katherine22@gmail.com>
2022-07-20 12:23:59 -0500

    lunch commit

--- js/promises.js
+++ js/promises.js
@@ -2,9 +2,22 @@
     const options = {
         method: 'GET',
         header: {
-            "Authorization": MY_OPEN_WEATHER_KEY
+            "Authorization": MY_GITHUB_API_KEY
         }
     };
+    const user = document.querySelector("#users");
+    const url = `https://api.github.com/users`
+    //this function is to fetch all usersnames
+    function getGitUserNames (){
+        return fetch(url, options)
+            .then(response => response.json())
+            .catch(function (error){
+                console.log("ERROR: " +error);
+            });
+    }
+    const users = await getGitUserNames();
+//    this function is to specific get inform about one username
+
 
 
 
