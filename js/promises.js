9b2b62b05e8c7eaf919beea25c344d2f52d38fc2
katygil7 <gil.katherine22@gmail.com>
2022-07-20 14:32:25 -0500

    commit

--- js/promises.js
+++ js/promises.js
@@ -7,7 +7,13 @@
     };
     const user = document.querySelector("#users");
     const url = `https://api.github.com/users`
+    //make the search bar work
+    const button = document.querySelector("#button");
+    const userInput = document.querySelector("#inputText")
+
+
     //this function is to fetch all usersnames
+
     function getGitUserNames (){
         return fetch(url, options)
             .then(response => response.json())
@@ -15,10 +21,29 @@
                 console.log("ERROR: " +error);
             });
     }
-    const users = await getGitUserNames();
+    const AllUsers = await getGitUserNames();
+
+
 //    this function is to specific get inform about one username
+    function getGithubUsernames() {
+        return fetch('https://api.github.com/users', options)
+            .then(response => response.json())
+            .catch(function(error) {
+                console.warn("ERROR: " + error);
+            });
+    }
+    //
+    function getGithubUsernameLastCommit(username) {
+        return fetch(`https://api.github.com/users/${username}/events/public`, options)
+            .then(response => response.json())
+            .catch(function(error) {
+                console.warn("ERROR: " + error);
+            });
+    }
 
+    const username = await getGithubUsernameLastCommit('katygil7')
 
+    console.log(username);
 
 
 })();
